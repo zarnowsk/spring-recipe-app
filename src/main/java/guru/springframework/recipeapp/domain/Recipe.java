52ba06f9f2e9ed1846b0b2bb171adfdc37398852
mzarnowskED <59625249+mzarnowskED@users.noreply.github.com>
2020-10-21 17:49:54 -0400

    Added Ingredient entity and relationships between Ingredient and Recipe

--- src/main/java/guru/springframework/recipeapp/domain/Recipe.java
+++ src/main/java/guru/springframework/recipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package guru.springframework.recipeapp.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -16,6 +17,8 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
@@ -101,5 +104,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
