882275e72f347ecdbe87bbee6052e149ad51b02b
mzarnowskED <59625249+mzarnowskED@users.noreply.github.com>
2020-10-22 17:33:23 -0400

    Fixed HashCode issues with Project Lombok.

--- src/main/java/guru/springframework/recipeapp/domain/Category.java
+++ src/main/java/guru/springframework/recipeapp/domain/Category.java
@@ -1,12 +1,14 @@
 package guru.springframework.recipeapp.domain;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Data
+@EqualsAndHashCode(exclude = {"recipes"})
 @Entity
 public class Category {
 
