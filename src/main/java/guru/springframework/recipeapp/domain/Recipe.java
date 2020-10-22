20906c958bee8e6938dc3acd30a5ef7966a4ba86
mzarnowskED <59625249+mzarnowskED@users.noreply.github.com>
2020-10-22 08:51:55 -0400

    Added Category entity and added relationship between Category and Recipe (many-to-many)

--- src/main/java/guru/springframework/recipeapp/domain/Recipe.java
+++ src/main/java/guru/springframework/recipeapp/domain/Recipe.java
@@ -24,6 +24,11 @@ public class Recipe {
     private Difficulty difficulty;
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+    joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     /* GETTERS & SETTERS */
 
@@ -121,5 +126,13 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
