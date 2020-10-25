9346fa1dccc659a93e4dfe76bd392f30acbb572a
mzarnowskED <59625249+mzarnowskED@users.noreply.github.com>
2020-10-25 10:14:57 -0400

    Added new HTML page to add/edit a recipe and added controller methods to handle viewing new page and receiving a POST from the new page.

--- src/main/java/guru/springframework/recipeapp/controllers/RecipeController.java
+++ src/main/java/guru/springframework/recipeapp/controllers/RecipeController.java
@@ -1,9 +1,12 @@
 package guru.springframework.recipeapp.controllers;
 
+import guru.springframework.recipeapp.commands.RecipeCommand;
 import guru.springframework.recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,9 +20,23 @@ public class RecipeController {
 
     @RequestMapping("/recipe/show/{id}")
     public String showById(@PathVariable String id, Model model) {
-
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
         return "/recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
